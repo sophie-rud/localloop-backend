0063d9fe929872aa849d74b57e62b1be7e90c829
sophie-rud <sophie.rudio@lapiscine.pro>
2026-01-12 00:49:33 +0100

    feat(middleware): add login validation middleware

---
+++ middlewares/login-middleware.js
@@ -0,0 +1,27 @@
+import bcrypt from 'bcrypt';
+import userService from '../services/UserService.js';
+
+async function loginMiddleware(req, res, next) {
+    try {
+        const { email, password } = req.body;
+        const user = await userService.getUserByEmail(email);
+
+        if (!user) {
+            return res.status(401).json({message: "Utilisateur non trouvé"});
+        }
+
+        if (!user.isActive) {
+            return res.status(403).json({message: "Votre compte a été suspendu"});
+        }
+
+        const isVerify = await bcrypt.compare(password, user.password);
+        if (!isVerify) return res.status(401).json({ message: "Non autorisé" });
+
+        req.user = user;
+        next();
+    } catch (err) {
+        next(err);
+    }
+}
+
+export default loginMiddleware;
